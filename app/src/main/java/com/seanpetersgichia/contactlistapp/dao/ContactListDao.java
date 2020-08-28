72e7c3291c47f0dc8c275e2a4f8236c49311d423
Peter-cloud-web <petergichia165@gmail.com>
2020-08-28 05:43:16 +0300

    Add the CRUD methods to the Dao interface class

--- app/src/main/java/com/seanpetersgichia/contactlistapp/dao/ContactListDao.java
+++ app/src/main/java/com/seanpetersgichia/contactlistapp/dao/ContactListDao.java
@@ -1,9 +1,32 @@
 package com.seanpetersgichia.contactlistapp.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.seanpetersgichia.contactlistapp.model.ContactListTable;
+
+import java.util.List;
 
 @Dao
 public interface ContactListDao {
 
+    @Insert
+    void insertContact(ContactListTable contactListTable);
+
+    @Update
+    void updateContact(ContactListTable contactListTable);
+
+    @Delete
+    void deleteContact(ContactListTable contactListTable);
+
+    @Query("DELETE FROM contactList_table")
+    void deleteAllContacts();
+
+    @Query("SELECT * FROM contactList_table")
+    LiveData<List<ContactListTable>> getAllContacts();
 
 }
