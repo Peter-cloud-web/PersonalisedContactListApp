b3bf417ee0ab7edfc8fd6db202caec4e861e4abe
Peter-cloud-web <petergichia165@gmail.com>
2020-09-03 10:11:29 +0300

    connect the adapter to the view model to get all the data from room

--- app/src/main/java/com/seanpetersgichia/contactlistapp/ui/MainActivity.java
+++ app/src/main/java/com/seanpetersgichia/contactlistapp/ui/MainActivity.java
@@ -1,17 +1,43 @@
 package com.seanpetersgichia.contactlistapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.seanpetersgichia.contactlistapp.R;
+import com.seanpetersgichia.contactlistapp.adapter.ContactListAdapter;
+import com.seanpetersgichia.contactlistapp.model.ContactListTable;
+import com.seanpetersgichia.contactlistapp.viewModel.ContactListViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+   private ContactListViewModel contactListViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final ContactListAdapter contactListAdapter = new ContactListAdapter();
+        recyclerView.setAdapter(contactListAdapter);
+
+        contactListViewModel = new ViewModelProvider(this).get(ContactListViewModel.class);
+        contactListViewModel.getAllContacts().observe(this, new Observer<List<ContactListTable>>() {
+            @Override
+            public void onChanged(List<ContactListTable> contactListTables) {
+                contactListAdapter.setContactList(contactListTables);
+            }
+        });
+
+
 
     }
 }
